8dc80325b5c11ab69f55b847c1606193b311ec79
Arunkrishna1998 <arunkvb98@gmail.com>
2024-01-10 15:50:16 +0530

    Making First Commit

---
+++ insyncbackend/asgi.py
@@ -0,0 +1,34 @@
+"""
+ASGI config for node_back project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insyncbackend.settings')
+django.setup()
+
+from .channelsmiddleware import JwtAuthMiddleware
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
+from chat import routing
+from post.routing import websocket_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insyncbackend.settings')
+
+django_asgi_application = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        'http': django_asgi_application,
+        'websocket': JwtAuthMiddleware(
+            AllowedHostsOriginValidator(URLRouter(routing.websocket_urlpatterns + websocket_urlpatterns))
+        )
+    }
+)
